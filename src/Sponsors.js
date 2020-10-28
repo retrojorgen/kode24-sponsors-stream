0c9a43aa85cf536a0c27aa37e1ec4899a8bf03dd
Jørgen Jacobsen <jorgeja@gmail.com>
2020-10-28 11:50:27 +0100

    sponsorlogos

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -18,13 +18,15 @@ import logo15 from "./images/sponsor/stacc-clean.png";
 import logo16 from "./images/sponsor/sweat-clean.png";
 import logo17 from "./images/sponsor/tripletex-clean.png";
 import logo18 from "./images/sponsor/ways-clean.png";
+import logo19 from "./images/sponsor/epinova-clean.png";
+import logo20 from "./images/sponsor/facilitate-clean.png";
 
 const Loop = keyframes`
     0% {
         transform: translateX(0)
     }
     100% {
-        transform: translateX(5052px)
+        transform: translateX(5600px)
     }
 `;
 const Scale = keyframes`
@@ -39,6 +41,8 @@ const Scale = keyframes`
     }
 `;
 
+console.log("hest");
+
 const SponsorsWrapper = styled.div`
   width: 100%;
   overflow: hidden;
@@ -47,7 +51,7 @@ const SponsorsWrapper = styled.div`
     padding: 20px 0;
     display: flex;
     animation: ${Loop} 120s linear infinite;
-    margin-left: -8204px;
+    margin-left: -5600px;
     flex-wrap: nowrap;
     .sponsor-logo {
       display: flex;
@@ -114,57 +118,69 @@ const SponsorsWrapper = styled.div`
         animation: ${Scale} 4s 1.8s ease-in-out infinite;
       }
       &:nth-child(19) {
-        animation: ${Scale} 4s 1.8s ease-in-out infinite;
+        animation: ${Scale} 4s 1.9s ease-in-out infinite;
       }
       &:nth-child(20) {
-        animation: ${Scale} 4s 1.7s ease-in-out infinite;
+        animation: ${Scale} 4s 2s ease-in-out infinite;
       }
       &:nth-child(21) {
-        animation: ${Scale} 4s 1.6s ease-in-out infinite;
+        animation: ${Scale} 4s 2s ease-in-out infinite;
       }
       &:nth-child(22) {
-        animation: ${Scale} 4s 1.5s ease-in-out infinite;
+        animation: ${Scale} 4s 1.9s ease-in-out infinite;
       }
       &:nth-child(23) {
-        animation: ${Scale} 4s 1.4s ease-in-out infinite;
+        animation: ${Scale} 4s 1.8s ease-in-out infinite;
       }
       &:nth-child(24) {
-        animation: ${Scale} 4s 1.3s ease-in-out infinite;
+        animation: ${Scale} 4s 1.7s ease-in-out infinite;
       }
       &:nth-child(25) {
-        animation: ${Scale} 4s 1.2s ease-in-out infinite;
+        animation: ${Scale} 4s 1.6s ease-in-out infinite;
       }
       &:nth-child(26) {
-        animation: ${Scale} 4s 1.1s ease-in-out infinite;
+        animation: ${Scale} 4s 1.5s ease-in-out infinite;
       }
       &:nth-child(27) {
-        animation: ${Scale} 4s 1s ease-in-out infinite;
+        animation: ${Scale} 4s 1.4s ease-in-out infinite;
       }
       &:nth-child(28) {
-        animation: ${Scale} 4s 0.9s ease-in-out infinite;
+        animation: ${Scale} 4s 1.3s ease-in-out infinite;
       }
       &:nth-child(29) {
-        animation: ${Scale} 4s 0.8s ease-in-out infinite;
+        animation: ${Scale} 4s 1.2s ease-in-out infinite;
       }
       &:nth-child(30) {
-        animation: ${Scale} 4s 0.7s ease-in-out infinite;
+        animation: ${Scale} 4s 1.1s ease-in-out infinite;
       }
       &:nth-child(31) {
-        animation: ${Scale} 4s 0.6s ease-in-out infinite;
+        animation: ${Scale} 4s 1s ease-in-out infinite;
       }
       &:nth-child(32) {
-        animation: ${Scale} 4s 0.5s ease-in-out infinite;
+        animation: ${Scale} 4s 0.9s ease-in-out infinite;
       }
       &:nth-child(33) {
-        animation: ${Scale} 4s 0.4s ease-in-out infinite;
+        animation: ${Scale} 4s 0.8s ease-in-out infinite;
       }
       &:nth-child(34) {
-        animation: ${Scale} 4s 0.3s ease-in-out infinite;
+        animation: ${Scale} 4s 0.7s ease-in-out infinite;
       }
       &:nth-child(35) {
-        animation: ${Scale} 4s 0.2s ease-in-out infinite;
+        animation: ${Scale} 4s 0.6s ease-in-out infinite;
       }
       &:nth-child(36) {
+        animation: ${Scale} 4s 0.5s ease-in-out infinite;
+      }
+      &:nth-child(37) {
+        animation: ${Scale} 4s 0.4s ease-in-out infinite;
+      }
+      &:nth-child(38) {
+        animation: ${Scale} 4s 0.3s ease-in-out infinite;
+      }
+      &:nth-child(39) {
+        animation: ${Scale} 4s 0.2s ease-in-out infinite;
+      }
+      &:nth-child(40) {
         animation: ${Scale} 4s 0.1s ease-in-out infinite;
       }
       &:last-item {
@@ -232,6 +248,12 @@ function Sponsors() {
           <img src={logo18} alt="logo" />
         </div>
         <div className="sponsor-logo">
+          <img src={logo19} alt="logo" />
+        </div>
+        <div className="sponsor-logo">
+          <img src={logo20} alt="logo" />
+        </div>
+        <div className="sponsor-logo">
           <img src={logo1} alt="logo" />
         </div>
         <div className="sponsor-logo">
@@ -284,6 +306,12 @@ function Sponsors() {
         </div>
         <div className="sponsor-logo">
           <img src={logo18} alt="logo" />
+        </div>
+        <div className="sponsor-logo">
+          <img src={logo19} alt="logo" />
+        </div>
+        <div className="sponsor-logo">
+          <img src={logo20} alt="logo" />
         </div>
       </div>
     </SponsorsWrapper>
