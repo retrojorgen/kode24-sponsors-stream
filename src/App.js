822286835b7c3ae6a4619255ebe6c7451bd6446b
Jørgen Jacobsen <jorgeja@gmail.com>
2021-01-06 09:53:38 +0100

    errors

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import styled from "styled-components";
 import Sponsors from "./Sponsors";
 
-import blackPixelPattern from "./images/black-pixel-pattern.png";
-import whitePixelPattern from "./images/white-pixel-pattern.png";
 const Wrapper = styled.div`
   width: 1920px;
   height: 1080px;
@@ -12,18 +10,6 @@ const Wrapper = styled.div`
   overflow: hidden;
 `;
 
-const MainWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-`;
-
-const TeaserWrapper = styled.div`
-  width: 100%;
-  height: calc(100% - 160px);
-  background-color: blue;
-  position: relative;
-`;
 const SponsorsWrapper = styled.div`
   height: 140px;
   display: flex;
